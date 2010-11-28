2e363a4bc2edfb01bdcb1b462af709ecea0b6dea
Daniel Hillenbrand <info@codeworkx.de>
2010-11-28 10:26:37 -0800

    added missing import

--- src/com/android/fm/radio/FMRadioService.java
+++ src/com/android/fm/radio/FMRadioService.java
@@ -28,6 +28,7 @@ import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.os.SystemProperties;
 
 import java.lang.ref.WeakReference;
 
